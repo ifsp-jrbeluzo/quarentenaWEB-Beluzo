fceb1d9f1323d3aaf06179c3058ac2f8f9609538
Amaliamelo <amali.melo@aluno.ifsp.edu.br>
2020-05-20 16:36:52 -0300

    Exercicio

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html >
+    <head>
+        <meta charset="utf-8">
+        <title>Exercico Compartilhado</title>
+    </head>
+    <body >
+        <h3> Exercicio Compartilhado</h3>
+    </body>
+</html>
